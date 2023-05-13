83771f433625f79f84a4659c0d0dcecbea7dbcba
adrianorezena <adrianocr@gmail.com>
2023-05-13 09:14:22 -0300

    add StoreAppNetwork & test plan

---
+++ StoreAppNetwork/Service/ProductService.swift
@@ -0,0 +1,8 @@
+//
+//  ProductService.swift
+//  StoreAppNetwork
+//
+//  Created by Adriano Rezena on 13/05/23.
+//
+
+import Foundation
---
+++ StoreAppNetworkTests/Service/ProductServiceTests.swift
@@ -0,0 +1,20 @@
+//
+//  ProductServiceTests.swift
+//  StoreAppNetworkTests
+//
+//  Created by Adriano Rezena on 13/05/23.
+//
+
+import XCTest
+
+final class ProductServiceTests: XCTestCase {
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+}
